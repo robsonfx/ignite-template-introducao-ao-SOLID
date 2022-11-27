5d96af56981c057f4c419c7313986d26286710fc
robsonfx <robsonfx@gmail.com>
2022-11-27 20:56:24 -0300

    ajustes swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
-import swaggerUi from "swagger-ui-express";
-import swaggerFile from "swagger.json";
-
 import { app } from ".";
-
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
